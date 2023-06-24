0e86f9be389cbe4178d1f9c68c0a9a0764563f41
unknown <github.yak@gmail.com>
2023-06-24 14:40:43 +0700

    update

--- tesModul_WithChoice.py
+++ tesModul_WithChoice.py
@@ -1,50 +1,61 @@
 import ramamath as rm
 
-print("=========== Modul by Arya Mwehehe ===========\n")
+print("========= Modul by Arya Mwehehe =========\n")
 
 def show_menu():
     print ("\n")
-    print("---------- MENU ----------")
+    print("----------------- MENU -----------------")
     print("[1] Volume Kubus")
     print("[2] Keliling Kubus")
     print("[3] Luas Kubus")
     print("[4] Luas Lingkaran")
     print("[5] Diameter Lingkaran")
+    print("[6] Keliling Persegi Panjang")
 
 show_menu()
 print("\n")
 
+
 choose = int(input("Masukkan Menu     : "))
+print("\n---------------- OPERASI ---------------")
 
 if choose == 1 :
-    angka = int(input("\nMasukkan Angka Volume              :  "))
+    angka = int(input("Masukkan Angka Volume              :  "))
     hasil_volume = rm.VolKubus(angka)
     print("Volume Kubus Adalah                : ", hasil_volume)
 
 if choose == 2:
-    angka = int(input("\nMasukkan Angka Keliling            :  "))
+    angka = int(input("Masukkan Angka Keliling            :  "))
     hasil_keliling = rm.KelKubus(angka)
     print("Keliling Kubus Adalah              : ", hasil_keliling)
 
 if choose == 3:
-    angka = int(input("\nMasukkan Angka Luas                :  "))
+    angka = int(input("Masukkan Angka Luas                :  "))
     hasil_luas = rm.LuasKubus(angka)
     print("Luas Kubus Adalah                  : ", hasil_luas)
 
 if choose == 4:
-    angka = int(input("\nMasukkan Angka Luas Lingkaran      :  "))
+    angka = int(input("Masukkan Angka Luas Lingkaran      :  "))
     hasil_LuasLingkaran = rm.LuasLingkaran(angka)
     print("Luas Lingkaran Adalah              : ", hasil_LuasLingkaran)
 
 if choose == 5:
-    angka = int(input("\nMasukkan Angka Diameter Lingkaran  :  "))
+    angka = int(input("Masukkan Angka Diameter Lingkaran  :  "))
     hasil_DiameterLingkaran = rm.DiameterLingkaran(angka)
     print("Diameter Lingkaran Adalah          : ", hasil_DiameterLingkaran)
 
-back = input("\nApakah Anda Ingin Mengoperasikan Kembali ?? (y/n) : ")
+if choose == 6:
+    panjang = int(input("Masukkan Panjang Persegi           :  "))
+    lebar   = int(input("Masukkan Lebar Persegi             :  "))
+    hasil_KelPersegiPanjang = rm.KelPersegiPanjang(panjang,lebar)
+    print("Keliling Persegi Panjang Adalah    : ", hasil_KelPersegiPanjang)
+
+print("\n----------------- DONE ----------------")
+back = input("Apakah Anda Ingin Mengoperasikan Kembali ?? (y/n) : ")
 
 if back == 'y':
     show_menu()
+    choose
 else:
     exit 
     print("\nAnda Telah Keluar Dari Program\n")
