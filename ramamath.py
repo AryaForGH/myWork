586e5d836e3e71604660dcf787927ee3e1db3d20
unknown <github.yak@gmail.com>
2023-06-27 08:30:00 +0700

    update

--- ramamath.py
+++ ramamath.py
@@ -13,5 +13,26 @@ def LuasLingkaran (num):
 def DiameterLingkaran (num):
     return 2 * num
 
+def LuasPersegiPanjang (p,l):
+    return p * l
+
 def KelPersegiPanjang (p,l):
     return 2 * (p + l)
+
+def Pertambahan (num1,num2):
+    return num1 + num2
+
+def Pengurangan (num1,num2):
+    return num1 - num2
+
+def Perkalian (num1,num2):
+    return num1 * num2
+
+def Pembagian (num1,num2):
+    return num1 / num2
+
+def LuasTrapesium (a,b,t):
+    return 1/2 * (a + b) * t 
+
+def LuasLayang (d1,d2):
+    return 1/2 * d1 * d2
--- tesModul_WithChoice.py
+++ tesModul_WithChoice.py
@@ -5,12 +5,19 @@ print("========= Modul by Arya Mwehehe =========\n")
 def show_menu():
     print ("\n")
     print("----------------- MENU -----------------")
-    print("[1] Volume Kubus")
-    print("[2] Keliling Kubus")
-    print("[3] Luas Kubus")
-    print("[4] Luas Lingkaran")
-    print("[5] Diameter Lingkaran")
-    print("[6] Keliling Persegi Panjang")
+    print("[1]  Volume Kubus")
+    print("[2]  Keliling Kubus")
+    print("[3]  Luas Kubus")
+    print("[4]  Luas Lingkaran")
+    print("[5]  Diameter Lingkaran")
+    print("[6]  Luas Persegi Panjang")
+    print("[7]  Keliling Persegi Panjang")
+    print("[8]  Pertambahan")
+    print("[9]  Pengurangan")
+    print("[10] Perkalian")
+    print("[11] Pembagian")
+    print("[12] Luas Trapesium")
+    print("[13] Luas Layang-Layang")
 
 show_menu()
 print("\n")
@@ -48,8 +55,51 @@ while back == "y":
     if choose == 6:
         panjang = int(input("Masukkan Panjang Persegi           :  "))
         lebar   = int(input("Masukkan Lebar Persegi             :  "))
+        hasil_LuasPersegiPanjang = rm.LuasPersegiPanjang(panjang,lebar)
+        print("Luas Persegi Panjang Adalah        : ", hasil_LuasPersegiPanjang)
+
+    if choose == 7:
+        panjang = int(input("Masukkan Panjang Persegi           :  "))
+        lebar   = int(input("Masukkan Lebar Persegi             :  "))
         hasil_KelPersegiPanjang = rm.KelPersegiPanjang(panjang,lebar)
         print("Keliling Persegi Panjang Adalah    : ", hasil_KelPersegiPanjang)
+
+    if choose == 8:
+        num1 = int(input("Masukkan Angka Pertama     :  "))
+        num2 = int(input("Masukkan Angka Kedua       :  "))
+        hasil_pertambahan = rm.Pertambahan(num1,num2)
+        print("Hasil Pertambahan Adalah   : ", hasil_pertambahan)
+
+    if choose == 9:
+        num1 = int(input("Masukkan Angka Pertama     :  "))
+        num2 = int(input("Masukkan Angka Kedua       :  "))
+        hasil_pengurangan = rm.Pengurangan(num1,num2)
+        print("Hasil Pengurangan Adalah   : ", hasil_pengurangan)
+
+    if choose == 10:
+        num1 = int(input("Masukkan Angka Pertama     :  "))
+        num2 = int(input("Masukkan Angka Kedua       :  "))
+        hasil_perkalian = rm.Perkalian(num1,num2)
+        print("Hasil Perkalian Adalah     : ", hasil_perkalian)
+
+    if choose == 11:
+        num1 = int(input("Masukkan Angka Pertama     :  "))
+        num2 = int(input("Masukkan Angka Kedua       :  "))
+        hasil_pembagian = rm.Pembagian(num1,num2)
+        print("Hasil Pembagian Adalah     : ", hasil_pembagian)
+
+    if choose == 12:
+        a = int(input("Masukkan Alas Trapesium   : "))
+        b = int(input("Masukkan Sisi Trapesium   : "))
+        t = int(input("Masukkan Tinggi Trapesium : "))
+        hasil_trapesium = rm.LuasTrapesium(a,b,t)
+        print("Hasil Luas Trapesium Adalah : ", hasil_trapesium)
+
+    if choose == 13:
+        d1 = int(input("Masukkan Diagonal Pertama : "))
+        d2 = int(input("Masukkan Diagonal Kedua   : "))
+        hasil_layang = rm.LuasLayang(d1,d2)
+        print("Hasil Luas Layang-Layang Adalah : ", hasil_layang)
 
     print("\n----------------- DONE ----------------")
 
